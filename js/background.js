b1de2b8731d5dd6da05bc94a53f928e21c4ff1e0
Lonee <48621431+itsnotLonee@users.noreply.github.com>
2019-11-28 14:19:22 +0100

    subiudaaa

--- js/background.js
+++ js/background.js
@@ -4,56 +4,102 @@ const sessions = document.getElementById("sessions-nav")
 const profile = document.getElementById("profile-nav")
 var bool = true
 
-if (window.screen.height > 1000) {
+var x = window.matchMedia("(min-width: 1000px)")
+myFunction(x)
+x.addListener(myFunction) 
 
-    friends.addEventListener('click', () => {
-        //alert("si");
-        var fbox = document.getElementById("friends-box")
-        if (bool == true){
-            fbox.style.gridColumn = "3 / 11"
-            fbox.style.gridRow = "2 / 6"
-            fbox.style.zIndex = "2"
-            bool = false
-        }
-        else if (bool == false){
-            fbox.style.gridColumn = "3 / 5"
-            fbox.style.gridRow = "2 / 4"
-            fbox.style.zIndex = "0"
-            bool = true
-        }
-    });
-    
-    sessions.addEventListener('click', () => {
-        //alert("si");
-        var slbox = document.getElementById("sessions-list-box")
-        if (bool == true) {
-            slbox.style.gridColumn = "3 / 11"
-            slbox.style.gridRow = "2 / 6"
-            slbox.style.zIndex = "2"
-            bool = false
-        } else {
-            slbox.style.gridColumn = "5 / 11"
-            slbox.style.gridRow = "2 / 6"
-            slbox.style.zIndex = "0"
-            bool = true
-        }
-    });
-    
-    profile.addEventListener('click', () => {
-        //alert("si");
-        var pbox = document.getElementById("profile-box")
-        if (pbox.style.display === "none") {
-            pbox.style.display = ""
-            pbox.style.gridColumn = "3 / 11"
-            pbox.style.gridRow = "2 / 6"
-            pbox.style.zIndex = "0"
-            bool = false
-        } else {
-            pbox.style.display = "none";
-            bool = true
-        }
-    });
+function myFunction(x) {
+    var fbox = document.getElementById("friends-box")
+    var slbox = document.getElementById("sessions-list-box")
+    var pbox = document.getElementById("profile-box")
+    fbox.style.width = '8.15em'
+    if (x.matches) { 
+       console.log("Mayor QUE 1000");
+       friends.addEventListener('click', () => {
+           if (bool == true) {
+               fbox.style.width = "40em"
+               fbox.style.height = ""
+               fbox.style.zIndex = "5"
+               bool = false
+           } else {
+               fbox.style.zIndex = "0"
+               fbox.style.width = '8.15em'
+               bool = true
+           }
+       });
 
+       sessions.addEventListener('click', () => {
+           if (bool == true) {
+               slbox.style.gridColumn = "3 / 11"
+               slbox.style.gridRow = "2 / 6"
+               slbox.style.zIndex = "2"
+               bool = false
+           } else {
+               slbox.style.gridColumn = "5 / 11"
+               slbox.style.gridRow = "2 / 6"
+               slbox.style.zIndex = "0"
+               bool = true
+           }
+       });
+
+       profile.addEventListener('click', () => {
+           if (pbox.style.display === "none") {
+               pbox.style.display = ""
+               pbox.style.gridColumn = "3 / 11"
+               pbox.style.gridRow = "2 / 6"
+               pbox.style.zIndex = "0"
+               bool = false
+           } else {
+               pbox.style.display = "none";
+               bool = true
+           }
+       })
+    } else {
+        /* ######################################## MAS PEQUEÑO ################################ */
+        console.log("es mas pequeño")
+        window.onresize = function (event) {
+            document.location.reload(true)
+        };
+        friends.addEventListener('click', () => {
+            if (bool == true) {
+                fbox.style.gridColumn = "3 / 11"
+                fbox.style.gridRow = "2 / 9"
+                fbox.style.zIndex = "2"
+                bool = false
+            } else {
+                fbox.style.gridColumn = "3 / 11"
+                fbox.style.gridRow = "2 / 4"
+                fbox.style.zIndex = "0"
+                bool = true
+            }
+        });
+
+        sessions.addEventListener('click', () => {
+            if (bool == true) {
+                slbox.style.gridColumn = "3 / 11"
+                slbox.style.gridRow = "2 / 9"
+                slbox.style.zIndex = "2"
+                bool = false
+            } else {
+                slbox.style.gridColumn = "3 / 11"
+                slbox.style.gridRow = "4 / 7"
+                slbox.style.zIndex = "0"
+                bool = true
+            }
+        });
+
+        profile.addEventListener('click', () => {
+            if (pbox.style.display === "none") {
+                pbox.style.display = ""
+                pbox.style.gridColumn = "3 / 11"
+                pbox.style.gridRow = "2 / 9"
+                pbox.style.zIndex = "0"
+                bool = false
+            } else {
+                pbox.style.display = "none";
+                bool = true
+            }
+        })
+    }
 }
-
-
+    
